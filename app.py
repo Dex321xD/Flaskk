6c1bae9af68ba3663af3bcac6efd1c1626955789
Dex321xD <xeronimus3012@gmail.com>
2025-02-21 02:25:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+import time
 
 app = Flask(__name__)
 
@@ -15,16 +18,17 @@ def obtener_precio_cobre():
     options.add_argument("--headless")  # Ejecutar en modo headless (sin interfaz)
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")  # Para evitar errores en contenedores
+    options.add_argument("--remote-debugging-port=9222")
 
     # Inicializar el controlador del navegador
-    driver = webdriver.Chrome(options=options)  # Asegúrate de tener ChromeDriver configurado
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
     try:
-        # Navegar a la página
         driver.get(url)
+        time.sleep(3)  # Esperar para asegurar la carga de la página
 
-        # Esperar hasta que el precio esté disponible
-        WebDriverWait(driver, 10).until(
+        WebDriverWait(driver, 15).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, 'div[data-test="instrument-price-last"]'))
         )
 
@@ -36,16 +40,12 @@ def obtener_precio_cobre():
     except Exception as e:
         return f"Error al obtener el precio del cobre: {e}"
     finally:
-        # Cerrar el navegador
         driver.quit()
 
 @app.route('/api/obtener_precio_cobre', methods=['GET'])
 def api_obtener_precio_cobre():
-    # Llamamos a la función que obtiene el precio
     precio = obtener_precio_cobre()
-    # Devolvemos el precio en formato JSON
     return jsonify({'precio_cobre': precio})
 
 if __name__ == "__main__":
-    # Arrancar la aplicación Flask en modo desarrollo
     app.run(debug=True, host="0.0.0.0", port=5000)
